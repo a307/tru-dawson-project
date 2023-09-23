dcd7c54b3c6e2c2a46e510bc523af3527720e25f
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-09-23 12:18:43 -0700

    Added firebase to branch

--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MaterialApp(home: CustomForm() //class
       ));
 }
