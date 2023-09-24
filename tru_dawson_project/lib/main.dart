31c0e6abb410b04944f1a2346112227104b1e365
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-09-24 13:13:00 -0700

    CustomForm now uses anonymous uid
    Anonymous UID is used from signInAnon() when submitting data to database

--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -4,91 +4,83 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:tru_dawson_project/authenticate.dart';
 import 'package:tru_dawson_project/database.dart';
 import 'firebase_options.dart';
+import 'package:tru_dawson_project/auth.dart';
 
 const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(home: Authenticate() //class
+  runApp(const MaterialApp(home: CustomForm() //class
       ));
 }
 
+class CustomForm extends StatefulWidget {
+  const CustomForm({super.key});
 
+  @override
+  State<CustomForm> createState() => _CustomFormState();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-// class CustomForm extends StatefulWidget {
-//   const CustomForm({super.key});
-
-//   @override
-//   State<CustomForm> createState() => _CustomFormState();
-// }
-
-// class _CustomFormState extends State<CustomForm> {
-//   final TextEditingController usernameTEC = TextEditingController();
-//   final TextEditingController passwordTEC = TextEditingController();
-
-//   final _formKey = GlobalKey<FormState>();
-//   String dropDownValue = list.first;
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Colors.blue,
-//       ),
-//       body: Form(
-//         key: _formKey,
-//         child: Column(
-//           children: <Widget>[
-//             const SizedBox(
-//               height: 20,
-//             ),
-//             Material(
-//               child: TextFormField(
-//                 controller: usernameTEC,
-//                 decoration: const InputDecoration(
-//                   icon: Icon(Icons.person),
-//                   hintText: 'Enter username',
-//                   labelText: 'Username',
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(
-//               height: 50,
-//             ),
-//             Material(
-//               child: TextFormField(
-//                 controller: passwordTEC,
-//                 decoration: const InputDecoration(
-//                   icon: Icon(Icons.password),
-//                   hintText: 'Enter a password',
-//                   labelText: 'Password',
-//                 ),
-//               ),
-//             ),
-//             const SizedBox(height: 50),
-//             ElevatedButton(
-//                 onPressed: () async {
-//                   await DatabaseService(uid: usernameTEC.text)
-//                       .updateUserData(usernameTEC.text, passwordTEC.text);
-
-//                   print(usernameTEC.text);
-//                   print(passwordTEC.text);
-//                 },
-//                 child: const Text('Submit'))
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class _CustomFormState extends State<CustomForm> {
+  final TextEditingController usernameTEC = TextEditingController();
+  final TextEditingController passwordTEC = TextEditingController();
+  final AuthService auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
+  String dropDownValue = list.first;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            Material(
+              child: TextFormField(
+                controller: usernameTEC,
+                decoration: const InputDecoration(
+                  hintText: 'Enter in text',
+                  labelText: 'Text',
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Material(
+              child: TextFormField(
+                controller: passwordTEC,
+                decoration: const InputDecoration(
+                  hintText: 'Enter a text',
+                  labelText: 'text',
+                ),
+              ),
+            ),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                onPressed: () async {
+                  dynamic result = await auth.signInAnon();
+                  if (result == null) {
+                    print('error signing in');
+                  } else {
+                    print('user has signed in');
+                    print(result.uid);
+                  }
+                  await DatabaseService(uid: result.uid)
+                      .updateUserData(usernameTEC.text, passwordTEC.text);
+                  print(usernameTEC.text);
+                  print(passwordTEC.text);
+                },
+                child: const Text('Submit'))
+          ],
+        ),
+      ),
+    );
+  }
+}
