82e5939975c62169d9e5167eecb692672889d101
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-09-24 10:56:02 -0700

    Added anonymous sign in

--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:tru_dawson_project/authenticate.dart';
 import 'package:tru_dawson_project/database.dart';
 import 'firebase_options.dart';
 
@@ -9,71 +10,85 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MaterialApp(home: CustomForm() //class
+  runApp(const MaterialApp(home: Authenticate() //class
       ));
 }
 
-class CustomForm extends StatefulWidget {
-  const CustomForm({super.key});
 
-  @override
-  State<CustomForm> createState() => _CustomFormState();
-}
 
-class _CustomFormState extends State<CustomForm> {
-  final TextEditingController usernameTEC = TextEditingController();
-  final TextEditingController passwordTEC = TextEditingController();
 
-  final _formKey = GlobalKey<FormState>();
-  String dropDownValue = list.first;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blue,
-      ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 20,
-            ),
-            Material(
-              child: TextFormField(
-                controller: usernameTEC,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'Enter username',
-                  labelText: 'Username',
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Material(
-              child: TextFormField(
-                controller: passwordTEC,
-                decoration: const InputDecoration(
-                  icon: Icon(Icons.password),
-                  hintText: 'Enter a password',
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            const SizedBox(height: 50),
-            ElevatedButton(
-                onPressed: () async {
-                  await DatabaseService(uid: usernameTEC.text)
-                      .updateUserData(usernameTEC.text, passwordTEC.text);
-                  print(usernameTEC.text);
-                  print(passwordTEC.text);
-                },
-                child: const Text('Submit'))
-          ],
-        ),
-      ),
-    );
-  }
-}
+
+
+
+
+
+
+
+
+
+
+// class CustomForm extends StatefulWidget {
+//   const CustomForm({super.key});
+
+//   @override
+//   State<CustomForm> createState() => _CustomFormState();
+// }
+
+// class _CustomFormState extends State<CustomForm> {
+//   final TextEditingController usernameTEC = TextEditingController();
+//   final TextEditingController passwordTEC = TextEditingController();
+
+//   final _formKey = GlobalKey<FormState>();
+//   String dropDownValue = list.first;
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//       ),
+//       body: Form(
+//         key: _formKey,
+//         child: Column(
+//           children: <Widget>[
+//             const SizedBox(
+//               height: 20,
+//             ),
+//             Material(
+//               child: TextFormField(
+//                 controller: usernameTEC,
+//                 decoration: const InputDecoration(
+//                   icon: Icon(Icons.person),
+//                   hintText: 'Enter username',
+//                   labelText: 'Username',
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(
+//               height: 50,
+//             ),
+//             Material(
+//               child: TextFormField(
+//                 controller: passwordTEC,
+//                 decoration: const InputDecoration(
+//                   icon: Icon(Icons.password),
+//                   hintText: 'Enter a password',
+//                   labelText: 'Password',
+//                 ),
+//               ),
+//             ),
+//             const SizedBox(height: 50),
+//             ElevatedButton(
+//                 onPressed: () async {
+//                   await DatabaseService(uid: usernameTEC.text)
+//                       .updateUserData(usernameTEC.text, passwordTEC.text);
+
+//                   print(usernameTEC.text);
+//                   print(passwordTEC.text);
+//                 },
+//                 child: const Text('Submit'))
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
