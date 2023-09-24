83264fa3444a3b499534725f525fbea3f92f5d2e
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-09-24 11:03:37 -0700

    Custom user with just uid

--- tru_dawson_project/lib/auth.dart
+++ tru_dawson_project/lib/auth.dart
@@ -1,13 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:tru_dawson_project/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  //create user object based on firebase user
+
+  DawsonUser? _userFromFirebaseUser(User? user) {
+    return user != null ? DawsonUser(uid: user.uid) : null;
+  }
 
   Future signInAnon() async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
     }
--- tru_dawson_project/lib/sign_in.dart
+++ tru_dawson_project/lib/sign_in.dart
@@ -30,7 +30,7 @@ class _SignInState extends State<SignIn> {
               print('error signing in');
             } else {
               print('user has signed in');
-              print(result);
+              print(result.uid);
             }
           },
         ),
