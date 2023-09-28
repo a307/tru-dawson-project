dc36f7cbd62cbfdc36fec5e30d817e9f9ddb8b2d
SpartanI117 <56614496+IsaiahB312@users.noreply.github.com>
2023-09-28 14:49:48 -0700

    Made a simple Form Generator
    - Added a button to generate a form from the text_field JSON file

--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -1,79 +1,111 @@
-import 'dart:convert';
-import 'package:flutter/services.dart';
-import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // Required for rootBundle to read files
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'dart:convert'; // Required for JSON operations
 
+// Entry point of the app (Didn't know what this meant before)
 void main() => runApp(const MyApp());
 
-class MyApp extends StatefulWidget {
+// Root Widget of the application
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
+    // MaterialApp provides Material Theming for the app (Didn't know what this meant before)
     return MaterialApp(
-      title: 'Form Generator',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const FormGenerator(),
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false, // This removes the debug banner from
+      home: MyHomePage(),
     );
   }
 }
 
-class FormGenerator extends StatefulWidget {
-  const FormGenerator({Key? key}) : super(key: key);
-
+// StatefulWidget allows UI to be rebuilt when state changes (Didn't know what this meant before)
+class MyHomePage extends StatefulWidget {
   @override
-  State<FormGenerator> createState() => _FormGeneratorState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _FormGeneratorState extends State<FormGenerator> {
-  List _items = [];
+// State class where logic and variables are defined
+class _MyHomePageState extends State<MyHomePage> {
+  // Key used to access form state and then validate (Kind of understand this but not completely)
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
 
-  void generateForm() async {
-    final String response =
-        await rootBundle.loadString('assets/text_field.json');
-    final data = await json.decode(response);
-    setState(() {
-      _items = [data];
-      print("number of items ${_items.length}");
+  // Variable to determine if form should be shown
+  bool showForm = false;
+
+  // Map to hold parsed JSON data
+  Map<String, dynamic> jsonMap = {};
+
+  // Initialize state: this method is called once when the widget is displayed
+  @override
+  void initState() {
+    super.initState();
+    // Load JSON from assets folder and then update state
+    _loadJsonFromAssets().then((value) {
+      setState(() {
+        jsonMap = value;
+      });
     });
   }
 
+  // Function to load and decode JSON from asset folder
+  Future<Map<String, dynamic>> _loadJsonFromAssets() async {
+    String jsonString = await rootBundle.loadString('assets/text_field.json');
+    return jsonDecode(jsonString);
+  }
+
+  // Build method for constructing the UI
   @override
   Widget build(BuildContext context) {
+    List<Widget> formFields = [];
+
+    // Check conditions to determine if FormBuilderTextField should be added
+    if (showForm && jsonMap['type'] == 'text_field') {
+      // If showForm is true and the type is text_field
+      // The type is text field so a text field will be generated
+      formFields.add(FormBuilderTextField(
+        name: jsonMap['meta_data'][
+            'control_name'], // This sets the name of the TextField and it's type of data. In this case the name of it will be the value of "control_name" which is "String" in the meta_data map (I'm still confused with what the meta_data represents)
+        decoration: const InputDecoration(
+            labelText:
+                'Text Field'), // This is placeholder or "hint" text for the text field
+      ));
+    }
+
+    // Returns the overall layout of this screen
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Generate Forms from JSON'),
-        backgroundColor: Colors.blue,
-      ),
-      body: Column(
-        children: [
-          const Expanded(
-            flex: 4,
-            child: Center(
-              child: Text('Placeholder'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Center(
-              child: MaterialButton(
-                color: Colors.blue,
-                onPressed: () => {generateForm()},
+      appBar: AppBar(title: const Text('Simple Form Generator')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Button to generate the form
+            Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    if (showForm == true) {
+                      showForm = false;
+                    } else {
+                      showForm = true;
+                    }
+                  });
+                },
                 child: const Text('Generate Form'),
-                textColor: Colors.white,
               ),
             ),
-          )
-        ],
+            // If showForm is true, show the form. Otherwise, show an empty container.
+            Expanded(
+              child: showForm
+                  ? FormBuilder(
+                      key: _fbKey,
+                      child: ListView(children: formFields),
+                    )
+                  : Container(),
+            ),
+          ],
+        ),
       ),
     );
   }
