4bec474cffec935e60975d0a4bfc8b67b475ee71
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-09-29 14:28:31 -0700

    Can now read data from Realtime Database in firebase, added comments

--- tru_dawson_project/lib/auth.dart
+++ tru_dawson_project/lib/auth.dart
@@ -8,14 +8,18 @@ class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   //create user object based on firebase user
 
+  //If user is not null, return DawsonUser with Uid
   DawsonUser? _userFromFirebaseUser(User? user) {
     return user != null ? DawsonUser(uid: user.uid) : null;
   }
 
+  //Call this to initiate a anonymous sign in (uid only)
   Future signInAnon() async {
     try {
+      //Call signInAnonymously to actually complete the sign in
       UserCredential result = await _auth.signInAnonymously();
       User? user = result.user;
+      //Call function to get DawsonUser from FirebaseUser
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -1,6 +1,7 @@
 //RUN THESE:
 //flutter pub add form_builder_validators
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
@@ -14,28 +15,35 @@ import 'sources/custom_fields.dart';
 import 'sources/signup_form.dart';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:tru_dawson_project/database.dart';
 import 'firebase_options.dart';
 import 'package:tru_dawson_project/auth.dart';
 
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
 void main() async {
+  //Ensures flutter widgets are initialized
   WidgetsFlutterBinding.ensureInitialized();
-  if (kIsWeb) {
-    //ISSUE: Inititlized wrong for windows, fix eventually
-    await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: 'AIzaSyChE23oQe0lYW_Y2TAKbCCjl1ox5yTikTc',
-            appId: "1:203503274066:web:d1b68e01a632af4186378b",
-            messagingSenderId: '203503274066',
-            projectId: "tru-dawson-project-2023"));
+  //Initialize Firebase
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyChE23oQe0lYW_Y2TAKbCCjl1ox5yTikTc',
+    appId: "1:203503274066:web:d1b68e01a632af4186378b",
+    messagingSenderId: '203503274066',
+    projectId: "tru-dawson-project-2023",
+    //Must be used to connect to Firebase Realtime database
+    databaseURL: 'https://tru-dawson-project-2023-default-rtdb.firebaseio.com/',
+  ));
+  //Connect to Firebase Real time database
+  final ref = FirebaseDatabase.instance.ref();
+  //get instance of json: simple_road_inspection
+  final snapshot = await ref.child('forms').get();
+  //Print data out if there is any
+  if (snapshot.exists) {
+    print(snapshot.value);
+  } else {
+    print('No data available.');
   }
-
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  //Run Application starting with MyApp as home
   runApp(const MaterialApp(home: MyApp() //class
       ));
 }
