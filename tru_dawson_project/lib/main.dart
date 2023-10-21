8797a5e3d986a366119a02cfbfe78300e0555843
chumplizz <48303627+elizabethranta@users.noreply.github.com>
2023-10-21 12:16:54 -0700

    added api key

--- tru_dawson_project/lib/main.dart
+++ tru_dawson_project/lib/main.dart
@@ -1,31 +1,51 @@
 //RUN THESE:
 //flutter pub add form_builder_validators
 // ignore_for_file: prefer_const_literals_to_create_immutables
-
+import 'map_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:tru_dawson_project/sign_in.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-// List to hold all of the individual JSONs
-List<Map<String, dynamic>>? separatedForms =
-    []; // List that contains each individual form
+// // List to hold all of the individual JSONs
+// List<Map<String, dynamic>>? separatedForms =
+//     []; // List that contains each individual form
 
-void main() async {
-  //Ensures flutter widgets are initialized
-  WidgetsFlutterBinding.ensureInitialized();
-  //Initialize Firebase
-  await Firebase.initializeApp(
-      options: const FirebaseOptions(
-    apiKey: 'AIzaSyChE23oQe0lYW_Y2TAKbCCjl1ox5yTikTc',
-    appId: "1:203503274066:web:d1b68e01a632af4186378b",
-    messagingSenderId: '203503274066',
-    projectId: "tru-dawson-project-2023",
-    //Must be used to connect to Firebase Realtime database
-    databaseURL: 'https://tru-dawson-project-2023-default-rtdb.firebaseio.com/',
-  ));
-  //Run Application starting with Generator as home
-  // runApp(MaterialApp(home: Generator(list, separatedForms) //class
-  //     ));
-  runApp(MaterialApp(home: SignIn() //class
-      ));
+// void main() async {
+//   //Ensures flutter widgets are initialized
+//   WidgetsFlutterBinding.ensureInitialized();
+//   //Initialize Firebase
+//   await Firebase.initializeApp(
+//       options: const FirebaseOptions(
+//     apiKey: 'AIzaSyChE23oQe0lYW_Y2TAKbCCjl1ox5yTikTc',
+//     appId: "1:203503274066:web:d1b68e01a632af4186378b",
+//     messagingSenderId: '203503274066',
+//     projectId: "tru-dawson-project-2023",
+//     //Must be used to connect to Firebase Realtime database
+//     databaseURL: 'https://tru-dawson-project-2023-default-rtdb.firebaseio.com/',
+//   ));
+//   //Run Application starting with Generator as home
+//   // runApp(MaterialApp(home: Generator(list, separatedForms) //class
+//   //     ));
+//   runApp(MaterialApp(home: MapScreen() //class
+//       ));
+// }
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+      title: 'Flutter Gps Task',
+      theme: ThemeData(
+        useMaterial3: true,
+         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home:  MapScreen(),
+    );
+  }
 }
--- tru_dawson_project/macos/Flutter/GeneratedPluginRegistrant.swift
+++ tru_dawson_project/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import firebase_auth
 import firebase_core
 import firebase_database
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -17,4 +18,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }
