bb87febc16d414046dd56f1a2f0169879e1bcdd3
chumplizz <48303627+elizabethranta@users.noreply.github.com>
2023-10-22 00:27:45 -0700

    Google map widget stuff
    Methods to get user location and display on a map as a widget

--- tru_dawson_project/lib/google_map.dart
+++ tru_dawson_project/lib/google_map.dart
@@ -1,3 +1,82 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
+class GoogleMapViewModel {
+  // A Completer for the GoogleMapController, which is used to interact with the Google Map widget.
+  Completer<GoogleMapController> controller = Completer();
 
+  // A method to load the current user's coordinates as a LatLng object.
+  Future<LatLng> loadCurrentUserCoordinates() async {
+    try {
+      // Check if GPS is enabled before attempting to fetch the location.
+      await _checkGpsEnabled();
+    } catch (ex) {
+      // Handle exceptions related to disabled location services and print debug information.
+      debugPrint(ex.toString());
+      throw Exception('Location services are disabled.');
+    }
+
+    Position? position;
+    try {
+      // Fetch the user's current location using Geolocator with a timeout.
+      position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best,
+        timeLimit: const Duration(seconds: 5),
+      );
+    } on TimeoutException catch (ex) {
+      // Handle timeout exceptions by printing debug information and using the last known position.
+      debugPrint(ex.toString());
+      position = await Geolocator.getLastKnownPosition();
+    }
+
+    if (position == null) {
+      // If the position is still null, it indicates a failed location capture.
+      throw Exception('Location capture failed. You might need to enable internet.');
+    }
+
+    // Return the user's coordinates as a LatLng object.
+    return LatLng(position.latitude, position.longitude);
+  }
+
+  // A private method to check if GPS is enabled and obtain necessary permissions.
+  Future<void> _checkGpsEnabled() async {
+    bool serviceEnabled;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled; prompt the user to enable them.
+      final locationOpened = await Geolocator.openLocationSettings();
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!locationOpened || !serviceEnabled) {
+        throw Exception('Location services are disabled.');
+      }
+    }
+
+    var permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      // If location permissions are denied, request the user's permission.
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        throw Exception('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // If permissions are denied forever, prompt the user to open app settings.
+      final openPermSettings = await Geolocator.openAppSettings();
+      if (!openPermSettings) {
+        throw Exception(
+          'Location permissions are permanently denied, we cannot request permissions.',
+        );
+      }
+    }
+
+    if (permission == LocationPermission.unableToDetermine) {
+      // Handle cases where location permissions are unable to be determined.
+      throw Exception('Unable to determine location permissions. Retry a few seconds later.');
+    }
+  }
+}
---
+++ tru_dawson_project/lib/google_map_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+import 'google_map.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class MapScreen extends StatefulWidget{
+  // This class represents the main screen of the app where the Google Map is displayed.
+  const MapScreen({super.key});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  // A Future to load the user's current coordinates from the view model. 
+  // This Future is used to fetch location information asynchronously.
+  late final Future<LatLng> _mapLoadedFuture;
+
+  // An instance of the GoogleMapViewModel class, responsible for managing map-related functionality.
+  final viewModel = GoogleMapViewModel();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the mapLoadedFuture by loading the user's current coordinates
+    _mapLoadedFuture = viewModel.loadCurrentUserCoordinates();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: FutureBuilder(
+              future: _mapLoadedFuture,
+              builder: (context, snapshot) {
+                // Check the connection state of the Future
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // Display a loading indicator while waiting for the coordinates.
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  // Handle and display an error message if an error occurs during fetching.
+                  return Center(
+                    child: Text(
+                      snapshot.error.toString(),
+                    ),
+                  );
+                }
+
+                // Once the coordinates are available, create a GoogleMapWidget to display the map.
+                return GoogleMapWidget(
+                  currentUserLocation: snapshot.data as LatLng,
+                  onMapCreated: (controller) {
+                    viewModel.controller.complete(controller);
+                  },
+                );
+              },
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'You are located in beanville',
+                  style: TextStyle(fontSize: 24),
+                ),
+                SizedBox(height: 16),
+                // Additional UI elements can be added here.
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class GoogleMapWidget extends StatelessWidget {
+  // This class represents the widget responsible for displaying the Google Map.
+  const GoogleMapWidget({
+    required this.onMapCreated,
+    required this.currentUserLocation,
+    super.key,
+  });
+
+  // Callback function to handle when the map is created.
+  final void Function(GoogleMapController) onMapCreated;
+  
+  // The user's current location on the map.
+  final LatLng currentUserLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return GoogleMap(
+      mapType: MapType.normal,
+      initialCameraPosition: CameraPosition(
+        // Set the initial camera position to focus on the user's current location.
+        target: currentUserLocation,
+        zoom: 18, // Zoom level for initial map view.
+      ),
+      onMapCreated: onMapCreated,
+      markers: {
+        Marker(
+          // Create a marker to represent the user's current location on the map.
+          markerId: const MarkerId('current_location'),
+          position: currentUserLocation,
+          onTap: () {
+            // Show a dialog with the latitude and longitude when the marker is tapped.
+            showDialog(
+              context: context,
+              builder: (_) => MarkerCoordinatesDialog(
+                latitude: currentUserLocation.latitude,
+                longitude: currentUserLocation.longitude,
+              ),
+            );
+          },
+        ),
+      },
+    );
+  }
+}
+
+class MarkerCoordinatesDialog extends StatelessWidget {
+  // This class represents a dialog that displays the latitude and longitude of a marker.
+  const MarkerCoordinatesDialog({
+    super.key,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  // Latitude and longitude values to be displayed in the dialog.
+  final double latitude, longitude;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("User's Current Location"),
+      content: Text(
+        // Display the latitude and longitude in the content of the dialog.
+        'Latitude: $latitude, Longitude: $longitude',
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('OK'),
+        ),
+      ],
+    );
+  }
+}
