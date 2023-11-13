1a00b8441664428a7943006d950032de7a4b38be
chumplizz <48303627+elizabethranta@users.noreply.github.com>
2023-11-13 01:05:38 -0800

    view submitted forms in progress

--- tru_dawson_project/lib/view_past_forms.dart
+++ tru_dawson_project/lib/view_past_forms.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'form_field.dart';
 import 'generation.dart';
+import 'package:intl/intl.dart'; // to turn the timestamp object back into a date
+import 'package:tuple/tuple.dart'; //to make tuples, for ordering "Inspector Name 5734892754893 1" -> tuple
+import 'package:image/image.dart'; // to load the images of previous forms from firebase to display
 
 class ViewPastForms extends StatefulWidget {
   final String globalEmail;
@@ -13,8 +16,8 @@ class ViewPastForms extends StatefulWidget {
 }
 
 class _ViewPastFormsState extends State<ViewPastForms> {
-  
-  final List<String> formTypes = globallist; // pulling actual list of forms from from generation.dart
+  // pulling actual list of forms from from generation.dart
+  final List<String> formTypes = globallist;
   // State variable to control form filtering
   bool showAllForms = false;
 
@@ -22,7 +25,7 @@ class _ViewPastFormsState extends State<ViewPastForms> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFC00205),
+        //backgroundColor: Color(0xFFC00205),
         title: Text('Past Submitted Forms'),
         actions: [
           // Toggle switch to show all forms or only user-submitted forms
@@ -124,7 +127,7 @@ class FormDetailsPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFC00205),
+        //backgroundColor: Color(0xFFC00205),
         title: Text('Form Details - $formType'), // Use the dynamic form type here
       ),
       body: Padding(
@@ -150,13 +153,44 @@ class FormDetailsPage extends StatelessWidget {
     );
   }
 
-  List<Widget> _buildFormDataWidgets(Map<String, dynamic> formData) {
-    List<Widget> widgets = [];
-    formData.forEach((key, value) {
-      widgets.add(FormFieldWidget(name: key, value: value));
-    });
-    return widgets;
-  }
+List<Widget> _buildFormDataWidgets(Map<String, dynamic> formData) {
+List<Tuple3<String, String, String>> formFields = [];
+print(formData);
+RegExp regex = RegExp(r'(.+?)\s(\d+)\s(.+)'); //to split all the name, long num, and short num
+//for each data piece in the submitted form,
+formData.forEach((key, value) {
+  RegExpMatch? match = regex.firstMatch(key); //match the regex to get the pieces
+  if (match != null) {
+    
+      String name = match.group(1) ?? '';
+      //String longNumber = match.group(2) ?? '';   //we don't need this num as of now, maybe later? 
+      String shortNumber = match.group(3) ?? '';
+      if(value is Timestamp){ //change timestamp to usable date
+          value = value.toDate();
+          value = DateFormat('yyyy-MM-dd HH:mm:ss').format(value);
+        }
+      if(value is List<dynamic>){ //if a picture url, fetch the pic from firebase and display it.
+        value = value.first.toString();
+      }
+      formFields.add(Tuple3(name, shortNumber, value));
+  } else {
+      // Handle cases where the regex doesn't match
+      print('Unmatched key: $key');
+    }
+});
+
+// Sort the form fields based on the short number
+formFields.sort((a, b) {
+  int shortNumberA = int.tryParse(a.item2) ?? 0;
+  int shortNumberB = int.tryParse(b.item2) ?? 0;
+  return shortNumberA.compareTo(shortNumberB);
+});
+
+// Generate widgets based on the sorted form fields
+return formFields.map((field) {
+  return Text('${field.item1}: ${field.item2}');
+}).toList();
+}
 
   String? extractDateFromFormId(String formId) {
     // Extract the date from the form ID (assuming the format is "YYYYMMDD-email")
