82303f0f941a10671c135e029ec682672d137e66
Sean <seanthomaslee17@gmail.com>
2023-11-12 16:52:14 -0800

    rest of form types displayed

--- tru_dawson_project/lib/view_past_forms.dart
+++ tru_dawson_project/lib/view_past_forms.dart
@@ -7,6 +7,9 @@ class ViewPastForms extends StatelessWidget {
 
   ViewPastForms({required this.globalEmail});
 
+  // List of form types (replace these with your actual collection names)
+  final List<String> formTypes = ['Sign Inspection', 'Fuel Usage', 'Truck Inspection', 'Equipment Inspection'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,17 +17,23 @@ class ViewPastForms extends StatelessWidget {
         backgroundColor: Color(0xFFC00205),
         title: Text('Past Submitted Forms'),
       ),
-      body: FutureBuilder<QuerySnapshot>(
-        future: FirebaseFirestore.instance.collection('Sign Inspection').get(),
+      body: FutureBuilder<List<QuerySnapshot>>(
+        // Fetch data for all form types concurrently
+        future: Future.wait(
+          formTypes.map((formType) =>
+              FirebaseFirestore.instance.collection(formType).get()),
+        ),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return CircularProgressIndicator();
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
-          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(child: Text('No forms available.'));
           } else {
-            var filteredDocs = snapshot.data!.docs
+            // Combine all form documents into a single list
+            var allDocs = snapshot.data!.expand((docs) => docs.docs).toList();
+
+            // Filter the documents based on the globalEmail
+            var filteredDocs = allDocs
                 .where((doc) => extractEmailFromFormId(doc.id) == globalEmail)
                 .toList();
 
@@ -37,7 +46,7 @@ class ViewPastForms extends StatelessWidget {
                 var email = extractEmailFromFormId(formId) ?? 'N/A';
 
                 return ListTile(
-                  title: Text('Sign Inspection'),
+                  title: Text('Form Type: ${getFormType(formDocument)}'),
                   subtitle: Text('Date Submitted: $dateSubmitted\nSubmitted by: $email'),
                   onTap: () {
                     Navigator.push(
@@ -57,25 +66,30 @@ class ViewPastForms extends StatelessWidget {
   }
 
   String? extractDateFromFormId(String formId) {
-    // Extract the date from the form ID (assuming the format is "YYYYMMDD-email")
     try {
       var dashIndex = formId.indexOf('-');
-      return formId.substring(dashIndex + 1); // Extract the date before the first dash
+      return formId.substring(dashIndex + 1);
     } catch (e) {
       return null;
     }
   }
 
   String? extractEmailFromFormId(String formId) {
-    // Extract the email from the form ID (assuming the format is "YYYYMMDD-email")
     try {
       var dashIndex = formId.indexOf('-');
-      return formId.substring(0, dashIndex); // Extract the email after the first dash
+      return formId.substring(0, dashIndex);
     } catch (e) {
       return null;
     }
   }
+
+  String getFormType(QueryDocumentSnapshot formDocument) {
+    // Extract form type from the document (you may need to adjust this based on your data structure)
+    // Assuming the form type is encoded in the collection name
+    return formDocument.reference.parent?.id ?? 'Unknown';
+  }
 }
+
 
 class FormDetailsPage extends StatelessWidget {
   final QueryDocumentSnapshot formDocument;
@@ -85,18 +99,19 @@ class FormDetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var formData = formDocument.data() as Map<String, dynamic>;
+    var formType = getFormType(formDocument);
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFC00205),
-        title: Text('Form Details'),
+        title: Text('Form Details - $formType'), // Use the dynamic form type here
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Sign Inspection'),
+            Text(formType), // Use the dynamic form type here
             SizedBox(height: 8),
             Text('Date Submitted: ${extractDateFromFormId(formDocument.id) ?? 'N/A'}'),
             SizedBox(height: 8),
@@ -140,5 +155,11 @@ class FormDetailsPage extends StatelessWidget {
     } catch (e) {
       return null;
     }
+  }
+
+  String getFormType(QueryDocumentSnapshot formDocument) {
+    // Extract form type from the document (you may need to adjust this based on your data structure)
+    // Assuming the form type is encoded in the collection name
+    return formDocument.reference.parent?.id ?? 'Unknown';
   }
 }
