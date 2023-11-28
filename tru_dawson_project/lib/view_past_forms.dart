fa48c64e66b5850676500cebe4891ef2c907b916
Kyle Bergman <60984164+bergmankyle99@users.noreply.github.com>
2023-11-27 22:28:39 -0800

    images now rendering in view past forms

--- tru_dawson_project/lib/view_past_forms.dart
+++ tru_dawson_project/lib/view_past_forms.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -6,7 +7,6 @@ import 'package:intl/intl.dart'; // to turn the timestamp object back into a dat
 import 'package:tuple/tuple.dart'; //to make tuples, for ordering "Inspector Name 5734892754893 1" -> tuple
 //this image package also has an instance of Color, which messes with everything. :/
 //import 'package:image/image.dart'; // to load the images of previous forms from firebase to display
-
 
 class ViewPastForms extends StatefulWidget {
   final String globalEmail;
@@ -60,7 +60,8 @@ class _ViewPastFormsState extends State<ViewPastForms> {
             var filteredDocs = showAllForms
                 ? allDocs
                 : allDocs
-                    .where((doc) => extractEmailFromFormId(doc.id) == widget.globalEmail)
+                    .where((doc) =>
+                        extractEmailFromFormId(doc.id) == widget.globalEmail)
                     .toList();
             return ListView.builder(
               itemCount: filteredDocs.length,
@@ -72,7 +73,8 @@ class _ViewPastFormsState extends State<ViewPastForms> {
 
                 return ListTile(
                   title: Text('Form Type: ${getFormType(formDocument)}'),
-                  subtitle: Text('Date Submitted: $dateSubmitted\nSubmitted by: $email'),
+                  subtitle: Text(
+                      'Date Submitted: $dateSubmitted\nSubmitted by: $email'),
                   onTap: () {
                     Navigator.push(
                       context,
@@ -115,7 +117,6 @@ class _ViewPastFormsState extends State<ViewPastForms> {
   }
 }
 
-
 class FormDetailsPage extends StatelessWidget {
   final QueryDocumentSnapshot formDocument;
 
@@ -133,17 +134,24 @@ class FormDetailsPage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
+        child: SingleChildScrollView(
+          // Wrap the Column with SingleChildScrollView
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(formType, style: const TextStyle(fontSize: 20,color: Colors.black),),
+              Text(
+                formType,
+                style: const TextStyle(fontSize: 20, color: Colors.black),
+              ),
               const SizedBox(height: 8),
-              Text('Date Submitted: ${extractDateFromFormId(formDocument.id) ?? 'N/A'}'),
+              Text(
+                  'Date Submitted: ${extractDateFromFormId(formDocument.id) ?? 'N/A'}'),
               const SizedBox(height: 8),
-              Text('Submitted by: ${extractEmailFromFormId(formDocument.id) ?? 'N/A'}'),
+              Text(
+                  'Submitted by: ${extractEmailFromFormId(formDocument.id) ?? 'N/A'}'),
               const SizedBox(height: 16),
-              const Text('Form Data:', style: TextStyle(fontSize: 20,color: Colors.black)),
+              const Text('Form Data:',
+                  style: TextStyle(fontSize: 20, color: Colors.black)),
               const SizedBox(height: 8),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -156,44 +164,45 @@ class FormDetailsPage extends StatelessWidget {
     );
   }
 
-
-List<Widget> _buildFormDataWidgets(Map<String, dynamic> formData) {
-List<Tuple3<String, String, String>> formFields = [];
-print(formData);
-RegExp regex = RegExp(r'(.+?)\s(\d+)\s(.+)'); //to split all the name, long num, and short num
+  List<Widget> _buildFormDataWidgets(Map<String, dynamic> formData) {
+    List<Tuple3<String, String, String>> formFields = [];
+    print(formData);
+    RegExp regex = RegExp(
+        r'(.+?)\s(\d+)\s(.+)'); //to split all the name, long num, and short num
 //for each data piece in the submitted form,
-formData.forEach((key, value) {
-  RegExpMatch? match = regex.firstMatch(key); //match the regex to get the pieces
-  if (match != null) {
-    
-      String name = match.group(1) ?? '';
-      //String longNumber = match.group(2) ?? '';   //we don't need this num as of now, maybe later? 
-      String shortNumber = match.group(3) ?? '';
-      if(value is Timestamp){ //change timestamp to usable date
+    formData.forEach((key, value) {
+      RegExpMatch? match =
+          regex.firstMatch(key); //match the regex to get the pieces
+      if (match != null) {
+        String name = match.group(1) ?? '';
+        //String longNumber = match.group(2) ?? '';   //we don't need this num as of now, maybe later?
+        String shortNumber = match.group(3) ?? '';
+        if (value is Timestamp) {
+          //change timestamp to usable date
           value = value.toDate();
           value = DateFormat('yyyy-MM-dd HH:mm:ss').format(value);
-        }
-       else if (value is List<dynamic>) {
-        // If a picture URL, fetch the pic from Firebase and display it.
-        if (value.isNotEmpty) {
-          value = value.first.toString();
-        } else {
-          value = 'No image'; // Handle the case where the list is empty
-        }
-      } else value ??= 'Field left blank.'; 
-      formFields.add(Tuple3(name, shortNumber, value));
-  } else {
-      // Handle cases where the regex doesn't match
-      print('Unmatched key: $key');
-    }
-});
+        } else if (value is List<dynamic>) {
+          // If a picture URL, fetch the pic from Firebase and display it.
+          if (value.isNotEmpty) {
+            value = value.first.toString();
+          } else {
+            value = 'No image'; // Handle the case where the list is empty
+          }
+        } else
+          value ??= 'Field left blank.';
+        formFields.add(Tuple3(name, shortNumber, value));
+      } else {
+        // Handle cases where the regex doesn't match
+        print('Unmatched key: $key');
+      }
+    });
 
 // Sort the form fields based on the short number
-formFields.sort((a, b) {
-  int shortNumberA = int.tryParse(a.item2) ?? 0;
-  int shortNumberB = int.tryParse(b.item2) ?? 0;
-  return shortNumberA.compareTo(shortNumberB);
-});
+    formFields.sort((a, b) {
+      int shortNumberA = int.tryParse(a.item2) ?? 0;
+      int shortNumberB = int.tryParse(b.item2) ?? 0;
+      return shortNumberA.compareTo(shortNumberB);
+    });
 
 // Generate widgets based on the sorted form fields
 // return formFields.map((field) {
@@ -201,56 +210,104 @@ formFields.sort((a, b) {
 // }).toList();
 
 // Create styled widgets
-List<Widget> styledWidgets = formFields.map((field) {
-  return Column(
-  children: [
-    Row(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: const Color.fromRGBO(192, 2, 5, 1),
-            borderRadius: BorderRadius.circular(9),
-            boxShadow: const [
-            BoxShadow(
-              color: Colors.grey, 
-              offset: Offset(0, 2), 
-              blurRadius: 4,
-            ),
-          ], 
+    List<Widget> styledWidgets = formFields.map((field) {
+      return Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(192, 2, 5, 1),
+                  borderRadius: BorderRadius.circular(9),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0, 2),
+                      blurRadius: 4,
+                    ),
+                  ],
+                ),
+                child: Text(
+                  '${field.item1}:',
+                  style: const TextStyle(
+                    color: Colors.white, // Set the text color for item1
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                  width: 8), // Add some spacing between the two parts
+              //if the slected image string (chrome) isnt null and platform is web, get image using Image.Network, otherwise display empty sizedbox
+              field.item3 != null &&
+                      field.item3
+                          .startsWith("https://firebasestorage.google") &&
+                      kIsWeb
+                  ? Image.network(
+                      field.item3,
+                      loadingBuilder: (BuildContext context, Widget child,
+                          ImageChunkEvent? loadingProgress) {
+                        if (loadingProgress == null) {
+                          return child;
+                        } else {
+                          return Center(
+                            child: CircularProgressIndicator(
+                              value: loadingProgress.expectedTotalBytes != null
+                                  ? loadingProgress.cumulativeBytesLoaded /
+                                      (loadingProgress.expectedTotalBytes ?? 1)
+                                  : null,
+                            ),
+                          );
+                        }
+                      },
+                      fit: BoxFit.contain,
+                      //Make photo only 100x100
+                      width: 100.0,
+                      height: 100.0,
+                    )
+                  : Text(
+                      field.item3,
+                      style: const TextStyle(
+                        color: Colors.black, // Set the text color for item3
+                        fontSize: 16,
+                      ),
+                    ),
+              // field.item3 != null &&
+              //         field.item3
+              //             .startsWith("https://firebasestorage.google") &&
+              //         kIsWeb
+              //     ? Container(
+              //         decoration: BoxDecoration(
+              //           image: DecorationImage(
+              //             image: NetworkImage(field.item3),
+              //             //whatever image you can put here
+              //             fit: BoxFit.cover,
+              //           ),
+              //         ),
+              //       )
+              //     : Text(
+              //         field.item3,
+              //         style: const TextStyle(
+              //           color: Colors.black, // Set the text color for item3
+              //           fontSize: 16,
+              //         ),
+              //       ),
+            ],
           ),
-          child: Text(
-            '${field.item1}:',
-            style: const TextStyle(
-              color: Colors.white, // Set the text color for item1
-              fontSize: 16,
-            ),
-          ),
-        ),
-        const SizedBox(width: 8), // Add some spacing between the two parts
-        Text(
-          field.item3,
-          style: const TextStyle(
-            color: Colors.black, // Set the text color for item3
-            fontSize: 16,
-          ),
-        ),
-      ],
-    ),
-    const SizedBox(height: 8), // Add some vertical spacing as a spacer
-  ],
-);
-}).toList();
+          const SizedBox(height: 8), // Add some vertical spacing as a spacer
+        ],
+      );
+    }).toList();
 
-return styledWidgets;
-
-}
+    return styledWidgets;
+  }
 
   String? extractDateFromFormId(String formId) {
     // Extract the date from the form ID (assuming the format is "YYYYMMDD-email")
     try {
       var dashIndex = formId.indexOf('-');
-      return formId.substring(dashIndex + 1); // Extract the date before the first dash
+      return formId
+          .substring(dashIndex + 1); // Extract the date before the first dash
     } catch (e) {
       return null;
     }
@@ -260,7 +317,8 @@ return styledWidgets;
     // Extract the email from the form ID (assuming the format is "YYYYMMDD-email")
     try {
       var dashIndex = formId.indexOf('-');
-      return formId.substring(0, dashIndex); // Extract the email after the first dash
+      return formId.substring(
+          0, dashIndex); // Extract the email after the first dash
     } catch (e) {
       return null;
     }
